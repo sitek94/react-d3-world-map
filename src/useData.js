e3b8e2b2b3e29b1fa8ceb6d28a3170f662f29a9d
Maciek Sitkowski <msitkowski94@gmail.com>
2020-09-24 12:48:59 +0200

    Add sphere projection, countries, interior borders and gradicule

--- src/useData.js
+++ src/useData.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { json } from 'd3';
-import { feature } from 'topojson';
+import { feature, mesh } from 'topojson';
 
 const jsonUrl =
   'https://unpkg.com/world-atlas@2.0.2/countries-50m.json';
@@ -12,9 +12,12 @@ export const useData = () => {
 
   useEffect(() => {
     json(jsonUrl)
-      .then(topojsonData => {
-        const { countries } = topojsonData.objects;
-        setData(feature(topojsonData, countries));
+      .then(topology => {
+        const { countries } = topology.objects;
+        setData({
+          countries: feature(topology, countries),
+          interiors: mesh(topology, countries, (a, b) => a !== b)
+        });
       });
   }, []);
 
